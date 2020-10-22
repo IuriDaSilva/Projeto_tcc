3e99316b7ff84e15d9e940e3d30dced3d3599c2f
IuriDaSilva <51327068+IuriDaSilva@users.noreply.github.com>
2020-10-22 17:06:15 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+
+	/*  Show/Hidden Submenus */
+	$('.nav-btn-submenu').on('click', function(e){
+		e.preventDefault();
+		var SubMenu=$(this).next('ul');
+		var iconBtn=$(this).children('.fa-chevron-down');
+		if(SubMenu.hasClass('show-nav-lateral-submenu')){
+			$(this).removeClass('active');
+			iconBtn.removeClass('fa-rotate-180');
+			SubMenu.removeClass('show-nav-lateral-submenu');
+		}else{
+			$(this).addClass('active');
+			iconBtn.addClass('fa-rotate-180');
+			SubMenu.addClass('show-nav-lateral-submenu');
+		}
+	});
+
+	/*  Show/Hidden Nav Lateral */
+	$('.show-nav-lateral').on('click', function(e){
+		e.preventDefault();
+		var NavLateral=$('.nav-lateral');
+		var PageConten=$('.page-content');
+		if(NavLateral.hasClass('active')){
+			NavLateral.removeClass('active');
+			PageConten.removeClass('active');
+		}else{
+			NavLateral.addClass('active');
+			PageConten.addClass('active');
+		}
+	});
+
+	/*  Exit system buttom */
+	$('.btn-exit-system').on('click', function(e){
+		e.preventDefault();
+		Swal.fire({
+			title: 'Você deseja sair do sistema?',
+			text: "Você irá encerrar a sessão e voltará para tela inicial.",
+			type: 'question',
+			showCancelButton: true,
+			confirmButtonColor: '#3085d6',
+			cancelButtonColor: '#d33',
+			confirmButtonText: 'Sim, sair!',
+			cancelButtonText: 'Não, cancelar'
+		}).then((result) => {
+			if (result.value) {
+				window.location="index.html";
+			}
+		});
+	});
+});
+(function($){
+    $(window).on("load",function(){
+        $(".nav-lateral-content").mCustomScrollbar({
+        	theme:"light-thin",
+        	scrollbarPosition: "inside",
+        	autoHideScrollbar: true,
+        	scrollButtons: {enable: true}
+        });
+        $(".page-content").mCustomScrollbar({
+        	theme:"dark-thin",
+        	scrollbarPosition: "inside",
+        	autoHideScrollbar: true,
+        	scrollButtons: {enable: true}
+        });
+    });
+})(jQuery);
